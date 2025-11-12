98af75f5b6bf15962665e2236ec43510eb7a0aa5
Anuj chib <137476284+anujchib@users.noreply.github.com>
2025-11-12 23:37:05 +0530

    heroku

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://disk-algo-simulator-b-production.up.railway.app'; // <- CHANGE THIS!
+const BASE_URL = 'https://disk-scheduler-7f3f3bd52476.herokuapp.com/'; // <- CHANGE THIS!
 
 const form = document.getElementById('scheduler-form');
 const chartCanvas = document.getElementById('seek-chart');
